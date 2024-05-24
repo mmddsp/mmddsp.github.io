3c552bc7adc625cac54c04b671ed766b03b68f21
mmddsp <3563860@gmail.com>
2024-05-24 19:36:31 +0800

    Update base.js

--- mmyy/base.js
+++ mmyy/base.js
@@ -32,8 +32,7 @@ const emails = ['@gmail.com',];
 const newestUrls = ['https://鈑娩睸.miaomiao.sbs/關於喵','https://鈑娩睸.miaomiao.sbs/關於喵'];
 const otherUrls = ['https://鈑娩睸.miaomiao.sbs/關於喵'];
 const foreverUrls = ['mmyy.sbs'];
-const notices = ['* 發送郵件到 / 五分鐘左右會回復最新可進入地址,可能會進垃圾箱,註意查。',
-    '* 我們推薦PC和Andriod手機用戶使用Chrome(谷歌)瀏覽器訪問，iPhone用戶我們建立您使用手機自帶Safria瀏覽器訪問。','* 如果點擊進入後打不開本站，請換（電信或聯通）網絡在訪問本站，移動網絡頻繁屏蔽本站～。',];
+const notices = [ '* 我们推荐PC和Andriod手机用户使用Chrome(谷歌)浏览器访问，iPhone用户我们建立您使用手机自带Safria浏览器访问。 ','* 如果点击进入后打不开本站，请换（电信或联通）网络在访问本站，移动网络频繁屏蔽本站～。',];
 
 var mainElem = document.getElementById('main');
 var logoElem = document.createElement('div');
